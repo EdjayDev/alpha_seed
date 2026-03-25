5e4c503cc6bf0eb769ecfe512be9bea7eb7d353f
Edjay Jangao <edjay123123@gmail.com>
2026-03-25 22:23:35 +0800

    Updating Intro scene

--- game_scenes/game.gd
+++ game_scenes/game.gd
@@ -6,6 +6,10 @@ var time : Timer
 @onready var game_anim_player: AnimationPlayer = $animation_nodes/game_anim_player
 @onready var player: Player = $y_sort/Player
 var input_locked : bool = false
+var in_cutscene : bool = false
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await intro_cutscene()
@@ -16,11 +20,13 @@ func _process(_delta: float) -> void:
 	pass
 	
 func intro_cutscene()->void:
+	in_cutscene = true
 	input_locked = true
 	player.player_animation.play("intro")
 	game_anim_player.play("letterbox")
 	await game_anim_player.animation_finished
 	input_locked = false
+	in_cutscene = false
 
 func set_timer()->void:
 	
--- player/player.gd
+++ player/player.gd
@@ -1,24 +1,47 @@
 extends CharacterBody2D
 class_name Player
 
+@onready var player_sprite: Sprite2D = $player_sprite
+
 const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+var animation_direction : String = "down"
+var last_animation : String = ""
+var state : String = "idle"
 
 @onready var player_animation: AnimationPlayer = $player_animation
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var game : Main_Game = get_tree().get_root().get_node("Game")
-	if game.input_locked:
-		return
 	var direction := Vector2(Input.get_axis("Left", "Right"), Input.get_axis("Up", "Down"))
-	if direction:
-		player_animation.play("walk")
+	if not direction:
+		direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	if game:
+		if game.input_locked:
+			direction = Vector2.ZERO
+		if game.in_cutscene:
+			return
+	if direction != Vector2.ZERO:
+		state = "walk"
+		if abs(direction.x) > abs(direction.y):
+			animation_direction = "side"
+			if direction.x > 0:
+				player_sprite.scale.x = -1
+			else:
+				player_sprite.scale.x = 1.0 
+		else:
+			if direction.y > 0:
+				animation_direction = "down"
+			else:
+				animation_direction = "up"
 		velocity = direction.normalized() * SPEED
 	else:
-		player_animation.play("idle")
+		state = "idle"
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+	update_animation()
 	move_and_slide()
+
+func update_animation()->void:
+	var current_animation = state + "_" + animation_direction
+	if current_animation != last_animation:
+		player_animation.play(state + "_" + animation_direction)
+		last_animation = current_animation
